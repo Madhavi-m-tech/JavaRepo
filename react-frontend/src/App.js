42e9c9b65433daa09d42b74344df59d87fe948fb
madhavi <madhavimukkisa6@gmail.com>
2021-05-23 19:35:22 +0530

    My First Commit

---
+++ react-frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import FooterComponent from './components/FooterComponent'
+import HeaderComponent from './components/HeaderComponent'
+import ListEmployeeComponent from './components/ListEmployeeComponent'
+import CreateEmployeeComponent from './components/CreateEmployeeComponent'
+import UpdateEmployeeComponent from './components/UpdateEmployeeComponent'
+
+function App() {
+  return (
+    <div>
+      <Router>
+            <HeaderComponent />
+            <div className="container">
+              <switch>
+                <Route path="/" exact component={ListEmployeeComponent}></Route>
+                <Route path="/employees" component={ListEmployeeComponent}></Route>
+                <Route path = "/add-employee" component = {CreateEmployeeComponent}></Route>
+                <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route>
+              </switch>
+            </div>
+            <FooterComponent />
+      </Router>
+    </div>
+
+  );
+}
+
+export default App;
+
+
